05cbbeb51b9a03d16213be3a7360867fca92c13c
Oleks-Y <borith1111@gmail.com>
2020-04-30 23:59:37 +0300

    reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,14 @@ import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatDialogModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 import { FlexLayoutModule} from "@angular/flex-layout";
 
@@ -27,6 +34,7 @@ import {PromotionService} from './services/promotion.service';
 import {LeaderService} from './services/leader.service'
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -38,19 +46,31 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule, 
     FlexLayoutModule,
+    FormsModule,
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
 
+  ],
+  entryComponents:[
+    LoginComponent
   ],
   providers: [
     DishService,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
+import {MatDialog, MatDialogRef} from '@angular/material'
+import { registerContentQuery } from '@angular/core/src/render3/instructions';
+import { LoginComponent } from '../login/login.component';
+ @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor( public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  openLoginForm(){
+    this.dialog.open(LoginComponent, {width:'500px', height: '450px'});
   }
 
 }
